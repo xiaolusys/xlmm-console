ccca68e8c2b0787f2d24fd94b1c6f1e57dcafca2
Shawn Sit <shawn@xiaolu.so>
2016-07-12 10:05:30 +0800

    disable other menu

--- src/constants/menu.js
+++ src/constants/menu.js
@@ -1,18 +1,20 @@
 export default [{
-  name: '排期',
-  link: '/schedule',
-  icon: 'calendar',
-}, {
-  name: '订单',
-  link: '/order',
-  icon: 'book',
-  sub: [{
-    name: '订单列表',
-    link: '/order/list',
-    icon: '',
-  }, {
-    name: '退货列表',
-    link: '/order/refunds',
-    icon: '',
-  }],
-}];
+    name: '排期',
+    link: '/schedule',
+    icon: 'calendar',
+  },
+  // {
+  //  name: '订单',
+  //  link: '/order',
+  //  icon: 'book',
+  //  sub: [{
+  //    name: '订单列表',
+  //    link: '/order/list',
+  //    icon: '',
+  //  }, {
+  //    name: '退货列表',
+  //    link: '/order/refunds',
+  //    icon: '',
+  // }],
+  // }
+];
