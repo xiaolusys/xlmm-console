25c0e3c81923e2b334a184f3aa92b46b1414ff87
Shawn Sit <shawn@xiaolu.so>
2016-08-17 16:44:31 +0800

    bugfixs

--- src/modules/Modals/SupplierLib.js
+++ src/modules/Modals/SupplierLib.js
@@ -147,6 +147,7 @@ class SupplierLib extends Component {
   tableProps = () => {
     const self = this;
     const { suppliers } = this.props;
+    const { pageSize, page } = this.state;
     return {
       className: 'margin-top-sm',
       rowKey: (record) => (record.id),
@@ -158,6 +159,8 @@ class SupplierLib extends Component {
       },
       pagination: {
         total: suppliers.count || 0,
+        pageSize: pageSize,
+        current: page,
         showTotal: total => `共 ${total} 条`,
         showSizeChanger: true,
         onShowSizeChange(current, pageSize) {
