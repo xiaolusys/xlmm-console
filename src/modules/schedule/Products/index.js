02acbdd9232b4198fd3989126b6d4b68f929056a
Shawn Sit <shawn@xiaolu.so>
2016-08-29 19:55:09 +0800

    enhancement

--- src/modules/schedule/Products/index.js
+++ src/modules/schedule/Products/index.js
@@ -421,7 +421,7 @@ export class ProductsWithForm extends Component {
       key: 'operating',
       render: (text, record) => (
         <div>
-          <a target="_blank" href={`/apis/items/v1/product?supplier_id=${record.supplierId}&saleproduct=${record.saleProductId}`} disabled={schedule.lockStatus}>资料录入</a>
+          <a target="_blank" href={`/apis/items/v1/product?supplier_id=${record.supplierId}&saleproduct=${record.saleProductId}`} disabled={schedule.lockStatus || record.inProduct}>资料录入</a>
           <span className="ant-divider"></span>
           <a target="_blank" href={`/mm/add_aggregeta/?search_model=${record.modelId}`} disabled={schedule.lockStatus}>上传图片</a>
           <span className="ant-divider"></span>
