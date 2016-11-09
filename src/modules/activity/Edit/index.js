9177b9d7862d65d0ee5bc0027aee7f32f8f438cf
ahlinjie <1051631394@qq.com>
2016-11-09 10:09:11 +0800

    page change event func

--- src/modules/activity/Edit/index.js
+++ src/modules/activity/Edit/index.js
@@ -57,6 +57,7 @@ class Editactivity extends Component {
     theParamsVisible: 0,
     oldParamsVisible: true,
     actLinkInputVisible: false,
+    scheduleIdInputVisible: false,
   }
 
   componentWillMount() {
@@ -120,17 +121,50 @@ class Editactivity extends Component {
     console.log('ac type: ', value);
     if (value === 'topic') {
       this.setState({ actLinkInputVisible: false });
+      this.setState({ scheduleIdInputVisible: true });
     } else {
       this.setState({ actLinkInputVisible: true });
+      this.setState({ scheduleIdInputVisible: false });
     }
   }
-
+  onScheduleSelect = (value) => {
+    const self = this;
+    const { filters } = this.props;
+    for (let i = 0; i < filters.schedules.length; i++) {
+      if (filters.schedules[i].id === value) {
+        console.log(filters.schedules[i], value);
+        this.props.form.setFieldsValue({
+        startTime: moment(filters.schedules[i].upshelfTime).format('YYYY-MM-DD HH:mm:ss'),
+        endTime: moment(filters.schedules[i].offshelfTime).format('YYYY-MM-DD HH:mm:ss'),
+      });
+      }
+    }
+  }
   inputActLink = () => {
     const { actLinkInputVisible } = this.state;
     if (actLinkInputVisible) {
       return (
         <Form.Item {...this.formItemLayout()} label="活动链接">
           <Input />
+        </Form.Item>
+        );
+    }
+    return (
+      <div></div>
+      );
+  }
+
+  inputSchedule = () => {
+    const self = this;
+    const { scheduleIdInputVisible } = this.state;
+    const { form, filters } = this.props;
+    const { getFieldProps, getFieldValue } = this.props.form;
+    if (scheduleIdInputVisible) {
+      return (
+        <Form.Item {...this.formItemLayout()} label="专题排期">
+          <Select {...getFieldProps('scheduleId')} onSelect={this.onScheduleSelect} value={getFieldValue('scheduleId')}>
+            {filters.schedules.map((item) => (<Select.Option value={item.id}>ID: {item.id} | {item.upshelfTime} - {item.offshelfTime}</Select.Option>))}
+          </Select>
         </Form.Item>
         );
     }
@@ -145,7 +179,6 @@ class Editactivity extends Component {
 
   render() {
     const { prefixCls, activity, form, filters } = this.props;
-    console.log('filters:', filters, activity);
     const { getFieldProps, getFieldValue, setFieldsValue } = this.props.form;
     const { activitys } = this.state;
     return (
