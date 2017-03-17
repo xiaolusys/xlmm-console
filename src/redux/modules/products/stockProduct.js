1c5ced8397eab4abdb24df3d35bb0a53aafddb51
huangyan <huangyan@huangyandeiMac.local>
2017-03-17 19:46:57 +0800

    [bugfix]hy-2

--- src/redux/modules/products/stockProduct.js
+++ src/redux/modules/products/stockProduct.js
@@ -1,58 +1,42 @@
 import createReducer from 'redux/createReducer';
 import { apisBase, scheduleTypes } from 'constants';
-import { fetchSku } from 'redux/modules/products/sku';
 
 const initialState = {
+  items: [],
+  count: 0,
 };
 
-const name = 'PRODUCT';
+const name = 'PRODUCTS';
 
 export default createReducer({
+  [`FETCH_${name}_REQUEST`]: (state, { payload, status }) => ({
+    ...state,
+    ...status,
+  }),
   [`FETCH_${name}_SUCCESS`]: (state, { payload, status }) => ({
     ...state,
     ...status,
-    ...payload.data,
+    items: payload.data,
   }),
   [`FETCH_${name}_FAILURE`]: (state, { payload, status }) => ({
     ...state,
     ...status,
   }),
-  [`CREATE_${name}_SUCCESS`]: (state, { payload, status }) => ({
-    ...state,
-    ...status,
-    updated: true,
-  }),
-  [`UPDATE_${name}_SUCCESS`]: (state, { payload, status }) => ({
-    ...state,
-    ...status,
-    updated: true,
-  }),
 }, initialState);
 
-export const createProduct = (params) => ({
+export const fetchProducts = (filters) => ({
   url: `${apisBase.item}stock_product`,
-  method: 'post',
-  type: `CREATE_${name}`,
-  data: {
-    ...params,
-  },
-});
-
-export const updateProduct = (productId, params) => ({
-  url: `${apisBase.item}stock_product/${productId}`,
-  method: 'put',
-  type: `UPDATE_${name}`,
-  data: {
-    ...params,
-  },
-});
-
-export const fetchProduct = (id) => ({
-  url: `${apisBase.item}stock_product/${id}`,
   method: 'get',
   type: `FETCH_${name}`,
+  params: {
+    ...filters,
+  },
 });
-
-export const resetProduct = () => ({
-  type: `RESET_${name}`,
+export const deleteProduct = (id) => ({
+  url: `${apisBase.item}stock_product/${id}`,
+  method: 'delete',
+  type: `DELETE_${name}`,
+  success: (resp, dispatch) => {
+    dispatch(fetchProducts());
+  },
 });
